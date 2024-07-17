7d1113ec8eaa3f020d226fd04eb8d7077d293c85
Krishan Mistry <kvjmistry@gmail.com>
2024-07-17 12:00:25 -0500

    cosmetics

--- ATPC/job/run_simulations.sh
+++ ATPC/job/run_simulations.sh
@@ -2,10 +2,8 @@
 
 # Define arrays
 # voltages=(2500 3000 3500 4000 5000 6000 7000 8000)
-# fieldMaps=(2 4 6 8 10 12 14 16 18 20)
-
 voltages=(2500)
-fieldMaps=(2)
+fieldMaps=(2 4 6 8 10 12 14 16 18 20)
 
 SCRIPT=ATPC_job.sh
 
